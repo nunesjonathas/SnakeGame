ff692fe63d51bc1f9b09e353bfb338a2ebb41611
nunesjonathas <nunes.jonathas@gmail.com>
2021-03-24 01:45:22 -0300

    Upando arquivos

--- script.js
+++ script.js
@@ -7,8 +7,15 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
+
 function criarBG() {
-    context.fillStyle = "red";
+    context.fillStyle = "lightgray";
     context.fillRect(0, 0, 16 * box, 16 *box);
 }
 
@@ -17,8 +24,68 @@ function criarCobrinha(){
         context.fillStyle = 'green';
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
-
 }
 
-criarBG();
-criarCobrinha();
+function drawFood(){
+    context.fillStyle = "black";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right")  direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function iniciarJogo(){
+    console.log(snake[0].x +" " +  snake[0].y);
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x  && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Gave Over :(');
+        }
+    }
+
+
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 * box && direction =="left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction =="down") snake[0].y = 0;
+    if(snake[0].y < 0 * box && direction =="up") snake[0].y =  16 * box;
+
+    
+    criarBG();
+    criarCobrinha();
+    drawFood();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    /*Movimento*/
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
+    /*Remove último componente */
+    
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 500);
